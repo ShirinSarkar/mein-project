3689e900f507fa917a8a6ac34e721ef88d629b02
ShirinSarkar <55285139+ShirinSarkar@users.noreply.github.com>
2019-10-07 09:13:38 +0200

    mein-project

---
+++ js/script2.js
@@ -0,0 +1,235 @@
+function iClick(){
+
+  document.getElementById("iSection").style = "right:60%"; 
+   
+}
+
+function nnClick(){
+ 
+    document.getElementById("nnSection").style = "right:59%";
+    
+ }
+//  ---------------------------------
+ 
+function tClick(){
+ 
+   document.getElementById("tSection").style = "right:55%";
+    
+ }
+
+ function hhClick(){
+
+    document.getElementById("hhSection").style = "right:54%"; 
+     
+  }
+  
+  function eeClick(){
+   
+      document.getElementById("eeSection").style = "right:53%";
+      
+   }
+   //------------------------------
+   
+  function eClick(){
+   
+     document.getElementById("eSection").style = "right:49%";
+      
+   }
+  
+   function nClick(){
+
+    document.getElementById("nSection").style = "right:48%"; 
+     
+  }
+  
+  function dClick(){
+   
+      document.getElementById("dSection").style = "right:47%";
+      
+   }
+   //--------------------- khate dovom------------------------------------------------------------
+   
+ 
+
+
+
+function i2Click(){
+
+    document.getElementById("i2Section").style = "right:64%"; 
+     
+  }
+
+  //------------------------
+  function wClick(){
+   
+      document.getElementById("wSection").style = "right:60%";
+      
+   }
+   
+  function oClick(){
+   
+     document.getElementById("oSection").style = "right:59%";
+      
+   }
+  
+   function uClick(){
+  
+      document.getElementById("uSection").style = "right:58%"; 
+       
+    }
+    
+    function lClick(){
+     
+        document.getElementById("lSection").style = "right:57%";
+        
+     }
+     
+    function d2Click(){
+     
+       document.getElementById("d2Section").style = "right:56%";
+        
+     }
+//-----------------------------------
+
+    
+     function lClick(){
+  
+      document.getElementById("lSection").style = "right:52%"; 
+       
+    }
+    
+    function i3Click(){
+     
+        document.getElementById("i3Section").style = "right:51%";
+        
+     }
+     
+    function k2Click(){
+     
+       document.getElementById("k2Section").style = "right:50%";
+        
+     }
+    
+
+    function e2Click(){
+
+    document.getElementById("e2Section").style = "right:49%"; 
+     
+  }
+
+  //----------------------------------------------------
+  function t2Click(){
+   
+      document.getElementById("t2Section").style = "right:45%";
+      
+   }
+   
+  function o2Click(){
+   
+     document.getElementById("o2Section").style = "right:44%";
+      
+   }
+  //    ------------------------------------------------------     
+   function t3Click(){
+  
+      document.getElementById("t3Section").style = "right:40%"; 
+       
+    }
+    
+    function h2Click(){
+     
+        document.getElementById("h2Section").style = "right:39%";
+        
+     }
+     
+    function aClick(){
+     
+       document.getElementById("aSection").style = "right:38%";
+        
+     }
+
+
+    
+     function n2Click(){
+  
+      document.getElementById("n2Section").style = "right:37%"; 
+       
+    }
+    
+    function k3Click(){
+     
+        document.getElementById("k3Section").style = "right:36%";
+        
+     }
+   
+// ------------------------------------------khate sevom----------------------------------
+  
+
+function a2Click(){
+   
+    document.getElementById("a2Section").style = "right:60%";
+     
+  }
+ 
+function l2Click(){
+
+   document.getElementById("l2Section").style = "right:59%"; 
+    
+ }
+
+ function l3Click(){
+  
+     document.getElementById("l3Section").style = "right:58%";
+     
+  }
+  //-------------------------------------------------------------------
+ function o3Click(){
+  
+    document.getElementById("o3Section").style = "right:54%";
+     
+  }
+ 
+  function fClick(){
+ 
+     document.getElementById("fSection").style = "right:53%"; 
+      
+   }
+   //--------------------------------------------------------------------------
+   
+   function yClick(){
+    
+       document.getElementById("ySection").style = "right:49%";
+       
+    }
+    
+   function o4Click(){
+    
+      document.getElementById("o4Section").style = "right:48%";
+       
+    }
+
+    function u2Click(){
+ 
+     document.getElementById("u2Section").style = "right:47%"; 
+      
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+//  ---------------------------------------
+
